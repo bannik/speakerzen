01cbf880642b122628c423e9abcbc4e0738b8bac
spidey_nr <spidey_nr@hotmail.com>
2013-10-09 03:39:38 +0300

    The actual working .php files on server

--- post_answer.php
+++ post_answer.php
@@ -4,39 +4,24 @@
 
 			$hostname="localhost";
 			$user = "root";
-			$pass = "";
-			$choice=$_POST["choice"];
-
-			if($choice==1)
-			{
-				$ch="choicea";
-			}
-			if($choice==2)
-			{
-				$ch="choiceb";
-			}
-			if($choice==3)
-			{
-				$ch="choicec";
-			}
-			if($choice==4)
-			{
-				$ch="choiced";
-			}
-			if($choice==5)
-			{
-				$ch="choicee";
-			}
-
-
+			$pass = "1234";
 			$id=$_POST['id']-1;
+			$choice=$_POST['choice'];
+			$freetext=$_POST['text'];
+			if($choice==0){
+				$ch="no";
+			}
+			else if($choice==1){
+				$ch="yes";
+			}
 			try {
 				$db = new PDO("mysql:host=$hostname;dbname=qs", $user, $pass,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 				$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+				if(!$freetext){
 					$sql = "UPDATE `answers` SET `$ch` = `$ch` +1 WHERE q_id =$id";
 					$speakers = $db->query($sql);
 					$result = $speakers->fetch();
-
+				}
 					$db = null; // close the database connection
 
 				}catch(PDOException $e)
