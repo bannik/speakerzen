e7ded47033450c6f796778e7a5f2ddaa8194ef73
spidey_nr <spidey_nr@hotmail.com>
2013-10-10 14:07:05 +0300

    Javascript for detailed page

---
+++ templates/app/js/detailed.js
@@ -0,0 +1,60 @@
+jQuery(document).ready(function($) {
+	function getURLParameter(name) {
+	  return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null
+	}
+
+	var qid = getURLParameter('qid');
+
+	$.ajax({
+		url: 'http://83.212.125.48/speakerzen/get_answers.php',
+		type: 'GET',
+		dataType: 'json',
+		data: {qid: qid},
+	})
+	.done(function(data) {
+		var x=parseInt(data["yes"]);
+		var y=parseInt(data["no"]);
+			avg=x+y;
+			if(x>y){
+				percent=(x/avg)*100;
+					$('.chartyesin').css({
+                   				height: percent+"%"
+                   			});
+					percent=100-percent;
+					$('.chartnoin').css({
+                   				height: percent+"%"
+                   			});
+
+			}
+			if(y>x){
+				percent=(y/avg)*100;
+					$('.chartyesin').css({
+                   				height: percent+"%"
+                   			});
+					percent=100-percent;
+					$('.chartnoin').css({
+                   				height: percent+"%"
+                   			});
+
+			}
+			if(y==x){
+
+					$('.chartyesin').css({
+                   				height: "50%"
+                   			});
+					$('.chartnoin').css({
+                   				height: "50%"
+                   			});
+
+			}
+			$(".boxlatest > .right").html("<p>"+data['yes']+"</p><p>"+data['no']+"</p>");
+
+	})
+	.fail(function() {
+		console.log("error");
+	})
+	.always(function() {
+		console.log("complete");
+	});
+
+});
