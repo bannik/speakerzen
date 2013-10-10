d813d309f9723b6ae1f7f384923fbd1672e8f353
spidey_nr <spidey_nr@hotmail.com>
2013-10-10 12:14:06 +0300

    Fixed some bugs. Now you can get all talks and their questions respectively. Thank the Rain Man for that

--- templates/app/js/custom.js
+++ templates/app/js/custom.js
@@ -1,4 +1,6 @@
-		var uid=1;
+$(document).ready(function($) {
+
+	var uid=1;
 		var tid=1;
 		function get_Answers(qid,count){
 		    	$.ajax({
@@ -12,27 +14,39 @@
 				var y=parseInt(data["no"]);
 				avg=x+y;
 				if(x>y){
-					percent=(x/avg);
+					percent=(x/avg)*100;
 
 					$('#chartin'+count).css({
-                    				height: percent*100
+                    				height: percent+"%"
                     			});
-					percent=percent*100;
+
 					$('#chartin'+count).html(Math.floor(percent)+"%");
 				}
-				// else{
-				// 	percent=(y/avg);
-				// 	$('#chartin'+count).css({
-    //                 				height: percent*100,
-    //                 				'background-color':'#e05d5d'
-    //                 			});
-				// 	$('#chartout'+count).css({
-    //                 				'border-color':'#e05d5d'
-    //                 			});
-					
-				// }
-				
-				
+				else if(y>x){
+					percent=(y/avg)*100;
+					$('#chartin'+count).css({
+                    				height: percent+"%",
+                    				'background-color':'#e05d5d'
+                    			});
+					$('#chartout'+count).css({
+                    				'border-color':'#e05d5d'
+                    			});
+					$('#chartin'+count).html(Math.floor(percent)+"%");
+
+				}
+				else
+				{
+
+					$('#chartin'+count).css({
+                    				height: "50%",
+                    				'background-color':'#19acca'
+                    			});
+					$('#chartout'+count).css({
+                    				'border-color':'#19acca'
+                    			});
+					$('#chartin'+count).html("50%");
+				}
+
 	        })
 			.fail(function() {
 				console.log("error");
@@ -50,29 +64,18 @@
 			})
 			.done(function(data) {
 				$('#accordion2').html('');
-				if(data["talks_title"])
-					{
-						$("#accordion2").append('<div class="accordion-group"><div class="accordion-heading"><a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion2" href="#collapseOne">'+data["talks_title"]+'</a></div><div id="collapseOne" class="accordion-body collapse" style="height: 0px;"><div class="accordion-inner" ><ul id="question"><a href="#"><li><div class="span8"><p>Do you like bacon?</p></div><div class="span3"><div class="chartout"><div class="chartin">70%</div></div></div></li></a><a href="#"><li><div class="span8"><p id="qesttitle">Do you like bacon?</p></div><div class="span3"><div class="chartout"><div class="chartin">70%</div></div></div></li></a><a href="#"><li><div class="span8"><p>Do you like bacon?</p></div><div class="span3"><div class="chartout"><div class="chartin">70%</div></div></div></li></a></ul></div></div></div>');
-						}
-						else{
-	                    	for (i in data)
+	                  	for (i in data)
 							{
 								if (data.hasOwnProperty(i))
 								{
-									
 	                    			$("#accordion2").prepend('<div class="accordion-group"><div class="accordion-heading"><a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion2" href="#collapse'+i+'">'+data[i]["talks_title"]+'</a></div><div id="collapse'+i+'" class="accordion-body collapse" style="height: 0px;"><div class="accordion-inner" ><ul id="question'+i+'"><a href="#"><li><div class="span8"><p>Do you like bacon?</p></div><div class="span3"><div class="chartout"><div class="chartin">70%</div></div></div></li></a><a href="#"><li><div class="span8"><p id="qesttitle">Do you like bacon?</p></div><div class="span3"><div class="chartout"><div class="chartin">70%</div></div></div></li></a><a href="#"><li><div class="span8"><p>Do you like bacon?</p></div><div class="span3"><div class="chartout"><div class="chartin">70%</div></div></div></li></a></ul></div></div></div>');
+	                    				get_Questions(tid,i);
+	                    				tid++;
 								}
 	                    	}
-	                    }	
-                	})
-			.fail(function() {
-				console.log("error");
-			})
-			.always(function() {
-				console.log("complete");
-			});
+	                    })
 		}
-		function get_Questions(talk_id){
+		function get_Questions(talk_id,count_qid){
 		    	$.ajax({
 				url: 'http://83.212.125.48/speakerzen/get_questions.php',
 				type: 'GET',
@@ -80,13 +83,13 @@
 				data:{tid:talk_id}
 			})
 			.done(function(data) {
-				$("#question").html('');
+				$("#question"+count_qid).html('');
                     	for (i in data)
 						{
 							if (data.hasOwnProperty(i))
 							{
-                    			$("#question").append('<a href="#"><li><div class="span8"><p>'+data[i]["question_title"]+'</p></div><div class="span3"><div class="chartout"><div class="chartin" id="chartin'+i+'">80%</div></div></div></li></a>');
-                    			get_Answers(data[i]['question_id'],i);
+                    			$("#question"+count_qid).append('<a href="#"><li><div class="span8"><p>'+data[i]["question_title"]+'</p></div><div class="span3"><div class="chartout" id="chartout'+talk_id+i+'"><div class="chartin" id="chartin'+talk_id+i+'">80%</div></div></div></li></a>');
+                    			get_Answers(data[i]['question_id'],talk_id+i);
 							}
                     	}
 
@@ -99,4 +102,4 @@
 			});
 		}
 		get_Talks();
-		get_Questions(tid);
+});
