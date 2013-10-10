9557cb33cc7d80a6fe860c121f88c5e50500f8d4
spidey_nr <spidey_nr@hotmail.com>
2013-10-10 15:23:58 +0300

    Live refresh and a fix in the bar percentages

--- templates/app/js/detailed.js
+++ templates/app/js/detailed.js
@@ -1,61 +1,64 @@
-jQuery(document).ready(function($) {
-	function getURLParameter(name) {
+function getURLParameter(name) {
 	  return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search)||[,""])[1].replace(/\+/g, '%20'))||null
 	}
 
-	var qid = getURLParameter('qid');
+var qid = getURLParameter('qid');
+function get_Results(){
+		$.ajax({
+			url: 'http://83.212.125.48/speakerzen/get_answers.php',
+			type: 'GET',
+			dataType: 'json',
+			data: {qid: qid},
+		})
+		.done(function(data) {
+			var x=parseInt(data["yes"]);
+			var y=parseInt(data["no"]);
+				avg=x+y;
+				if(x>y){
+					percent=(x/avg)*100;
+						$('.chartyesin').css({
+	                   				height: percent+"%"
+	                   			});
+						percent=100-percent;
+						$('.chartnoin').css({
+	                   				height: percent+"%"
+	                   			});
 
-	$.ajax({
-		url: 'http://83.212.125.48/speakerzen/get_answers.php',
-		type: 'GET',
-		dataType: 'json',
-		data: {qid: qid},
-	})
-	.done(function(data) {
-		var x=parseInt(data["yes"]);
-		var y=parseInt(data["no"]);
-			avg=x+y;
-			if(x>y){
-				percent=(x/avg)*100;
-					$('.chartyesin').css({
-                   				height: percent+"%"
-                   			});
-					percent=100-percent;
-					$('.chartnoin').css({
-                   				height: percent+"%"
-                   			});
+				}
+				if(y>x){
+					percent=(y/avg)*100;
+						$('.chartnoin').css({
+	                   				height: percent+"%"
+	                   			});
+						percent=100-percent;
+						$('.chartyesin').css({
+	                   				height: percent+"%"
+	                   			});
 
-			}
-			if(y>x){
-				percent=(y/avg)*100;
-					$('.chartyesin').css({
-                   				height: percent+"%"
-                   			});
-					percent=100-percent;
-					$('.chartnoin').css({
-                   				height: percent+"%"
-                   			});
+				}
+				if(y==x){
 
-			}
-			if(y==x){
+						$('.chartyesin').css({
+	                   				height: "50%"
+	                   			});
+						$('.chartnoin').css({
+	                   				height: "50%"
+	                   			});
 
-					$('.chartyesin').css({
-                   				height: "50%"
-                   			});
-					$('.chartnoin').css({
-                   				height: "50%"
-                   			});
+				}
+				$(".boxlatest > .left").html("<p class='yes_p'>Yes</p><p class='no_p'>No</p>");
+				$(".boxlatest > .right").html("<p>"+data['yes']+"</p><p>"+data['no']+"</p>");
 
-			}
-			$(".boxlatest > .left").html("<p class='yes_p'>Yes</p><p class='no_p'>No</p>");
-			$(".boxlatest > .right").html("<p>"+data['yes']+"</p><p>"+data['no']+"</p>");
-
-	})
-	.fail(function() {
-		console.log("error");
-	})
-	.always(function() {
-		console.log("complete");
-	});
+		})
+		.fail(function() {
+			console.log("error");
+		})
+		.always(function() {
+			console.log("complete");
+		});
+	}
+jQuery(document).ready(function($) {
+	get_Results();
+	setInterval( "get_Results()", 5000 );
 
 });
